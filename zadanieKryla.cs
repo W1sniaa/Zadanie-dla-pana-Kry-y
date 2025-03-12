a01541566a24e115112abab865f6739a4701bb27
W1sniaa <167750719+W1sniaa@users.noreply.github.com>
2025-03-12 11:24:42 +0100

    Update zadanieKryla.cs

--- zadanieKryla.cs
+++ zadanieKryla.cs
@@ -21,13 +21,14 @@ namespace zadanie_5__kryła_
                     suma += podanaLiczba;
                 }
                 Console.WriteLine("Aktualna suma liczb nieparzystych: {0}", suma);
+                Console.WriteLine(" ");
 
-                if (suma % 100 == 0)
+                if (suma % 100 == 0 && suma != 0)
                 {
-                    Console.WriteLine("Pętla zakończyła swoje zadanie.");
+                    Console.WriteLine("Pętla zakończyła swoje zadanie (suma liczb nieparzystych jest podzielna przez 100).");
                 }
             }
-            while (suma % 100 != 0);
+            while (suma % 100 != 0 || suma == 0);
         }
     }
 }
